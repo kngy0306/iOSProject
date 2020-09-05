93e409c43401491612cd4014b7a79e366f69c0a3
小長谷祐人 <kona@kona.local>
2020-09-05 20:05:42 +0900

    create counterAPP

--- Practice/CounterApp/CounterApp/ContentView.swift
+++ Practice/CounterApp/CounterApp/ContentView.swift
@@ -9,9 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("\(number)")
+            Button(action: {self.number += 1}){
+                Text("カウント")
+            }
+        }
     }
+//    func text() -> Text{
+//        Text("カウント")
+//    }
+    
+//    func count(){
+//        self.number += 1
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
