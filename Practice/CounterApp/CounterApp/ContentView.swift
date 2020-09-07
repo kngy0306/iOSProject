dde4dd83b293a554ac01f7e79b4996f8401df353
Kona <kitami.kitano20@gmail.com>
2020-09-07 16:20:56 +0900

    タイププロパティ、タイプメソッド、列挙体

--- Practice/CounterApp/CounterApp/ContentView.swift
+++ Practice/CounterApp/CounterApp/ContentView.swift
@@ -19,9 +19,14 @@ struct ContentView: View {
                     .aspectRatio(contentMode: .fit)
                 Text("\(number)")
                     .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
             }
             Button(action: {self.number += 1}){
                 Text("カウント")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(10.0)
             }
         }
     }
